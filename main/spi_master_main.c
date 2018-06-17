c683c055b7b118e8ce4cb7a63524ffbced038826
Clément Decoodt <clement@decoodt.eu>
2018-06-18 00:05:31 +0200

    Add initial SPI device using DMA

---
+++ main/spi_master_main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/spi_master.h"
+#include "soc/gpio_struct.h"
+#include "driver/gpio.h"
+
+#define PIN_NUM_MOSI 23
+#define PIN_NUM_CLK  19
+#define PIN_NUM_CS   22
+
+// There are 5 8-bit shift registers per panel, 2 panels
+#define BUFFER_SIZE (5*8)
+
+// Place data into DRAM. Constant data gets placed into DROM by default, which is not accessible by DMA.
+DRAM_ATTR static uint8_t buffer_0[BUFFER_SIZE];
+DRAM_ATTR static uint8_t buffer_1[BUFFER_SIZE];
+
+// Display buffer. The write buffer is the other (0->1, 1->0)
+static int display_buffer_idx = 0;
+static uint8_t* display_buffer = buffer_0;
+
+// Transaction descriptors. Declared static so they're not allocated on the stack; we need this memory even when this
+// function is finished because the SPI driver needs access to it even while we're already calculating the next line.
+static spi_transaction_t trans_buf[2] = {
+   {
+      .flags = 0,
+      .cmd = 0,
+      .addr = 0,
+      .length = BUFFER_SIZE,
+      .rxlength = 0,
+      .tx_buffer = &buffer_0,
+      .rx_buffer = NULL,
+   },
+   { 
+      .flags = 0,
+      .cmd = 0,
+      .addr = 0,
+      .length = BUFFER_SIZE,
+      .rxlength = 0,
+      .tx_buffer = &buffer_1,
+      .rx_buffer = NULL,
+   }
+};
+
+static spi_transaction_t *last_buf_desc = &trans_buf[1];
+
+// Simple routine to generate some patterns and send them to the LED Panel.
+static void animate(spi_device_handle_t spi) {
+    int frame=0;
+    esp_err_t ret;
+
+    while(1) {
+        frame++;
+        // TODO Calculate the animation
+        for(int idx = 0; idx < BUFFER_SIZE; idx++) {
+           display_buffer[idx] = 128;
+        }
+
+        // Wait for last transmission to be successful before swapping buffers
+        ret=spi_device_get_trans_result(spi, &last_buf_desc, portMAX_DELAY);
+        assert(ret==ESP_OK);
+
+        // Swap buffers
+        last_buf_desc = &trans_buf[display_buffer_idx];
+        display_buffer_idx = 1 - display_buffer_idx;
+        display_buffer = display_buffer_idx == 0 ? buffer_0 : buffer_1;
+
+        // Send the data to the SPI device
+        ret=spi_device_queue_trans(spi, &trans_buf[display_buffer_idx], portMAX_DELAY);
+        assert(ret==ESP_OK);
+    }
+}
+
+void app_main() {
+    esp_err_t ret;
+
+    // SPI device configuration structs
+    spi_device_handle_t spi;
+    spi_bus_config_t buscfg={
+        .miso_io_num=-1,
+        .mosi_io_num=PIN_NUM_MOSI,
+        .sclk_io_num=PIN_NUM_CLK,
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1,
+        .max_transfer_sz=BUFFER_SIZE
+    };
+    spi_device_interface_config_t devcfg={
+        .clock_speed_hz=100*1000,           //Clock out at 100 kHz
+        .mode=0,                                //SPI mode 0
+        .spics_io_num=PIN_NUM_CS,               //CS pin
+        .queue_size=7,                          //We want to be able to queue 7 transactions at a time
+        .command_bits=0, // Do not use command/address, just send raw data to Shift Registers
+        .address_bits=0,
+        .dummy_bits=0
+    };
+
+    // Initialize the SPI bus with configuration
+    ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
+    ESP_ERROR_CHECK(ret);
+    ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+    ESP_ERROR_CHECK(ret);
+
+    // Do wolffy stuff
+    animate(spi);
+}
