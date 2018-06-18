04585b6429c6b36a0a9ed7dd691aafcbe6fabd1e
Clément Decoodt <clement@decoodt.eu>
2018-06-18 23:41:47 +0200

    Working panel (only 1 panel)

--- main/spi_master_main.c
+++ main/spi_master_main.c
@@ -13,7 +13,7 @@
 #define PIN_NUM_CS   5
 
 // There are 5 8-bit shift registers per panel, 2 panels
-#define BUFFER_SIZE (5*8)
+#define BUFFER_SIZE 5
 
 // Place data into DRAM. Constant data gets placed into DROM by default, which is not accessible by DMA.
 DRAM_ATTR static uint8_t buffer_0[BUFFER_SIZE];
@@ -22,6 +22,7 @@ DRAM_ATTR static uint8_t buffer_1[BUFFER_SIZE];
 // Display buffer. The write buffer is the other (0->1, 1->0)
 static int display_buffer_idx = 0;
 static uint8_t* display_buffer = buffer_0;
+static uint8_t* write_buffer = buffer_1;
 
 // Transaction descriptors. Declared static so they're not allocated on the stack; we need this memory even when this
 // function is finished because the SPI driver needs access to it even while we're already calculating the next line.
@@ -30,7 +31,7 @@ static spi_transaction_t trans_buf[2] = {
       .flags = 0,
       .cmd = 0,
       .addr = 0,
-      .length = BUFFER_SIZE,
+      .length = 8*BUFFER_SIZE,
       .rxlength = 0,
       .tx_buffer = &buffer_0,
       .rx_buffer = NULL,
@@ -39,14 +40,14 @@ static spi_transaction_t trans_buf[2] = {
       .flags = 0,
       .cmd = 0,
       .addr = 0,
-      .length = BUFFER_SIZE,
+      .length = 8*BUFFER_SIZE,
       .rxlength = 0,
       .tx_buffer = &buffer_1,
       .rx_buffer = NULL,
    }
 };
 
-static spi_transaction_t *last_buf_desc = &trans_buf[1];
+static spi_transaction_t *last_buf_desc = &trans_buf[0];
 
 static void pre_g(spi_transaction_t *t) {
    gpio_set_level(PIN_NUM_CS, 1);
@@ -58,72 +59,84 @@ static void post_g(spi_transaction_t *t) {
 
 // Simple routine to generate some patterns and send them to the LED Panel.
 static void animate(spi_device_handle_t spi) {
-    int frame=0;
-    esp_err_t ret;
+   int frame = 0;
+   int update_counter = 0;
+   esp_err_t ret;
 
-    while(1) {
-        // No spam :)
-        if(frame % (2 << 12) == 0)
-           printf("Frame %d animating!\n", frame);
+   while(1) {
+      // No spam :)
+      if(frame % (1 << 10) == 0) {
+         printf("frame %d scanning!\n", frame);
+         update_counter++;
+      }
 
-        // TODO Calculate the animation
-        for(int idx = 0; idx < BUFFER_SIZE; idx++) {
-           display_buffer[idx] = (frame+idx)%128;
-        }
+      for (int line = 0; line < 8; line++) {
 
-        // Wait for last transmission to be successful before swapping buffers
-        if(frame != 0) {
-           ret=spi_device_get_trans_result(spi, &last_buf_desc, portMAX_DELAY);
-           assert(ret==ESP_OK);
-        }
+         // TODO Calculate the animation
+         for(int idx = 0; idx < BUFFER_SIZE-1; idx++) {
+            write_buffer[idx] = (idx == 0)*(1<<update_counter%8);
+         }
 
-        // Swap buffers
-        last_buf_desc = &trans_buf[display_buffer_idx];
-        display_buffer_idx = 1 - display_buffer_idx;
-        display_buffer = display_buffer_idx == 0 ? buffer_0 : buffer_1;
+         // Scan line
+         write_buffer[4] = 1<<line;
 
-        // Send the data to the SPI device
-        ret=spi_device_queue_trans(spi, &trans_buf[display_buffer_idx], portMAX_DELAY);
-        assert(ret==ESP_OK);
-        frame++;
-    }
+         // Wait for last transmission to be successful before swapping buffers
+         if(line != 0) {
+            ret=spi_device_get_trans_result(spi, &last_buf_desc, portMAX_DELAY);
+            assert(ret==ESP_OK);
+         }
+
+         // Swap buffers
+         display_buffer_idx = 1 - display_buffer_idx;
+         display_buffer = display_buffer_idx == 0 ? buffer_0 : buffer_1;
+         write_buffer = display_buffer_idx == 0 ? buffer_1 : buffer_0;
+         last_buf_desc = &trans_buf[1 - display_buffer_idx];
+
+         // Send the data to the SPI device
+         ret=spi_device_queue_trans(spi, &trans_buf[display_buffer_idx], portMAX_DELAY);
+         assert(ret==ESP_OK);
+      }
+
+      // New frame
+      frame++;
+   }
 }
 
 void app_main() {
-    esp_err_t ret;
+   esp_err_t ret;
 
-    printf("System initialization!\n");
-    // SPI device configuration structs
-    spi_device_handle_t spi;
-    spi_bus_config_t buscfg={
-        .miso_io_num=-1,
-        .mosi_io_num=PIN_NUM_MOSI,
-        .sclk_io_num=PIN_NUM_CLK,
-        .quadwp_io_num=-1,
-        .quadhd_io_num=-1,
-        .max_transfer_sz=BUFFER_SIZE
-    };
-    spi_device_interface_config_t devcfg={
-        .clock_speed_hz=100*1000,           //Clock out at 100 kHz
-        .mode=0,                                //SPI mode 0
-        .spics_io_num=-1,               //CS pin
-        .queue_size=7,                          //We want to be able to queue 7 transactions at a time
-        .command_bits=0, // Do not use command/address, just send raw data to Shift Registers
-        .address_bits=0,
-        .dummy_bits=0,
-        .pre_cb=pre_g,
-        .post_cb=post_g
-    };
+   printf("System initialization!\n");
+   // SPI device configuration structs
+   spi_device_handle_t spi;
+   spi_bus_config_t buscfg={
+      .miso_io_num=-1,
+      .mosi_io_num=PIN_NUM_MOSI,
+      .sclk_io_num=PIN_NUM_CLK,
+      .quadwp_io_num=-1,
+      .quadhd_io_num=-1,
+      .max_transfer_sz=8
+   };
+   spi_device_interface_config_t devcfg={
+      .clock_speed_hz=100*1000,  //Clock out at 100 kHz
+      .mode=0,                   //SPI mode 0
+      .spics_io_num=-1,          //CS pin
+      .queue_size=10,             //We want to be able to queue 10 transactions at a time
+      .command_bits=0,           // Do not use command/address, just send raw data to Shift Registers
+      .address_bits=0,
+      .dummy_bits=0,
+      .pre_cb=pre_g,
+      .post_cb=post_g
+   };
 
-    gpio_set_direction(PIN_NUM_CS, GPIO_MODE_OUTPUT);
+   gpio_set_direction(PIN_NUM_CS, GPIO_MODE_OUTPUT);
 
-    // Initialize the SPI bus with configuration
-    ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
-    ESP_ERROR_CHECK(ret);
-    ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
-    ESP_ERROR_CHECK(ret);
-    printf("SPI Bus initialized!\n");
+   // Initialize the SPI bus with configuration
+   ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
+   ESP_ERROR_CHECK(ret);
+   ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+   ESP_ERROR_CHECK(ret);
+   printf("SPI Bus initialized!\n");
 
-    // Do wolffy stuff
-    animate(spi);
+   // Do wolffy stuff
+   animate(spi);
 }
